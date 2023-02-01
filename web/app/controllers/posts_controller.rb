7e4e2372651d77bbd7c66b928d72bf35c2cebd29
Galshamir8 <galshamir8@gmail.com>
2023-02-01 20:04:42 +0200

    Correct strong parameters

--- web/app/controllers/posts_controller.rb
+++ web/app/controllers/posts_controller.rb
@@ -66,6 +66,6 @@ class PostsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def post_params
-    params.require(:post).permit(:user_id, :description, :attachments)
+    params.require(:post).permit(:description, :attachments)
   end
 end
