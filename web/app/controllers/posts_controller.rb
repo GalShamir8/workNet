4c51f0cc143d0d16cc50851348b6e56e04caf1ae
Galshamir8 <galshamir8@gmail.com>
2023-04-23 19:39:09 +0300

    Fix wrong instance variable

--- web/app/controllers/posts_controller.rb
+++ web/app/controllers/posts_controller.rb
@@ -68,7 +68,6 @@ class PostsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
-    @user = @post.user
   end
 
   # Only allow a list of trusted parameters through.
