ea56887bcdda3cd8fd951eeb0f8da20830d1be00
Galshamir8 <galshamir8@gmail.com>
2023-04-30 22:06:13 +0300

    Adding privacy policy

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users
 
   post 'posts/:id/like', to: 'posts#like', as: 'post_like'
+  get 'static_pages/privacy_policy', to: 'static_pages#privacy_policy'
   root 'posts#index'
 end
