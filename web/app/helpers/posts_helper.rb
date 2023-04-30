d8ed69a87a12280c0f2f31e6eae4593b73c8470f
Galshamir8 <galshamir8@gmail.com>
2023-04-30 17:29:09 +0300

    Add helper methods in post_hlper

--- web/app/helpers/posts_helper.rb
+++ web/app/helpers/posts_helper.rb
@@ -1,2 +1,23 @@
+# frozen_string_literal: true
+
+# PostsHelper
 module PostsHelper
+  def post_likes(post)
+    [
+      number_of_likes(post),
+      '&nbsp;',
+      like_icon(post)
+    ].join.html_safe
+  end
+
+  def like_icon(post)
+    # icon_class = post.likes.include?(current_user) ? 'glyphicon glyphicon-heart' : 'glyphicon glyphicon-heart-empty'
+    icon_class = post.user == current_user ? 'glyphicon glyphicon-heart' : 'glyphicon glyphicon-heart-empty'
+    "<i class='#{icon_class}'></i>"
+  end
+
+  def number_of_likes(post)
+    # post.likes.count
+    0
+  end
 end
