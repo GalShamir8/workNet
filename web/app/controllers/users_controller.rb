3591fe009c352799310cacd5caf0a57e290e9838
Galshamir8 <galshamir8@gmail.com>
2023-05-14 19:32:06 +0300

    Display only users from the same company

--- web/app/controllers/users_controller.rb
+++ web/app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.where(company: current_company)
   end
 
   # GET /users/1 or /users/1.json
