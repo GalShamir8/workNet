d47ba7b38a7275ce2e31a2febc2c837d1e164bd1
Galshamir8 <galshamir8@gmail.com>
2023-06-01 05:05:22 +0300

    Code format

--- web/app/controllers/users/registrations_controller.rb
+++ web/app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 # Override Devise Users::RegistrationsController to be able extend required logic
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :require_admin, only: %i[new]
-  prepend_before_action :require_no_authentication, only: [:create, :cancel]
+  prepend_before_action :require_no_authentication, only: %i[create cancel]
   def update
     super do
       unless params[resource_name][:profile_pictures].nil?
