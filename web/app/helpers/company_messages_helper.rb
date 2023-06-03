41f8b0fd82f5f7a840366485bdd244a335b944e6
Galshamir8 <galshamir8@gmail.com>
2023-06-03 17:32:19 +0300

    Add expires in icon with tool tip to company messages view

--- web/app/helpers/company_messages_helper.rb
+++ web/app/helpers/company_messages_helper.rb
@@ -3,4 +3,9 @@
 # CompanyMessagesHelper
 module CompanyMessagesHelper
   def time_options = %w[minutes hours days weeks months]
+
+  def expired_icon(company_message)
+    tool_tip = "This message will disaper in #{distance_of_time_in_words(company_message.expires_in - Time.now)}"
+    "<i class='glyphicon glyphicon-hourglass' title='#{tool_tip}'></i>".html_safe
+  end
 end
